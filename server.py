59ff50eef0adc9f10ed4683f34278e7f12be55c7
Baris Sayil <baris.sayil@insa-lyon.fr>
2020-07-17 21:35:20 +0200

    Update

--- server.py
+++ server.py
@@ -38,6 +38,8 @@ def sentiment():
 if __name__ == '__main__':
 	if args.model_name_or_path == '':
 		args.model_name_or_path == 'barissayil/bert-sentiment-analysis-sst'
+	#Configuration for the desired transformer model
+	config = AutoConfig.from_pretrained(args.model_name_or_path)
 	#Create the model with the desired transformer model
 	if args.model_type == 'bert':
 		model = BertForSentimentClassification.from_pretrained(model_name_or_path)
