91f60d14825ef78c2e28fe91765bce56f3db4f18
Baris Sayil <baris.sayil@insa-lyon.fr>
2020-07-19 11:37:08 +0200

    Add arguments info

--- arguments.py
+++ arguments.py
@@ -2,12 +2,12 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser()
 
-parser.add_argument('--maxlen_train', type=int, default=30)
-parser.add_argument('--maxlen_val', type=int, default=50)
-parser.add_argument('--batch_size', type=int, default=32)
-parser.add_argument('--lr', type=float, default=2e-5)
-parser.add_argument('--num_eps', type=int, default=2)
-parser.add_argument('--num_threads', type=int, default=1)
+parser.add_argument('--maxlen_train', type=int, default=30, help='Maximum number of tokens in the input sequence during training.')
+parser.add_argument('--maxlen_val', type=int, default=50, help='Maximum number of tokens in the input sequence during evaluation.')
+parser.add_argument('--batch_size', type=int, default=32, help='Batch size during training.')
+parser.add_argument('--lr', type=float, default=2e-5, help='Learning rate for Adam.')
+parser.add_argument('--num_eps', type=int, default=2, help='Number of training epochs.')
+parser.add_argument('--num_threads', type=int, default=1, help='Number of threads for collecting the datasets.')
 parser.add_argument('--output_dir', type=str, default='my_model', help='Where to save the trained model, if relevant.')
 parser.add_argument(
 	'--model_name_or_path', 
